bb9010a6e3fb84c478cdf92574aedf503ccd5e71
Igor Mikushkin <igor.mikushkin@gmail.com>
2021-06-02 20:13:49 +0400

    lint

--- chunks.go
+++ chunks.go
@@ -33,7 +33,7 @@ func Chunks(s string, chunkSize int) []string {
 	var chunks []string = make([]string, 0, (len(s)-1)/chunkSize+1)
 	currentLen := 0
 	currentStart := 0
-	for i, _ := range s {
+	for i := range s {
 		if currentLen == chunkSize {
 			chunks = append(chunks, s[currentStart:i])
 			currentLen = 0
